5d9e4f536a4713b0cfe66b8943bb2ee0e7168a25
temfa <akinfetemitope5@gmail.com>
2025-03-12 19:03:59 +0100

    Integrated with Paystack

---
+++ app/about/page.tsx
@@ -0,0 +1,27 @@
+import HomeAbout from "@/components/home-about";
+import React from "react";
+import styles from "../page.module.css";
+
+const About = () => {
+  return (
+    <>
+      <HomeAbout />
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className={styles.video}
+        data-aos="fade-right"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in"
+        data-aos-mirror="true"
+        data-aos-once="false">
+        <source src="/video2.MP4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </>
+  );
+};
+
+export default About;
