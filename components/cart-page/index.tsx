504c20fec5f0b72a08f5d0d9793914db624cf005
temfa <akinfetemitope5@gmail.com>
2025-03-12 18:01:00 +0100

    Integrated with Paystack

--- components/cart-page/index.tsx
+++ components/cart-page/index.tsx
@@ -23,47 +23,56 @@ const CartPage = () => {
           <p>Sub-total</p>
         </div>
         <div className={styles.body}>
-          {cartItems?.map((item, index) => {
-            return (
-              <div key={index} className={styles.single}>
-                <div className={styles.title}>
-                  <Image width={70} height={70} src={item.image} alt="Product" />
-                  <div>
-                    <h2>{item.title}</h2>
-                    <p>{item.size}</p>
-                    <p>{formatter(item.promoPrice)}</p>
+          {cartItems?.length === 0 ? (
+            <div className={styles.nothing}>
+              <p>You do not have any product in your cart!!</p>
+              <Link href="/products">Start Shopping</Link>
+            </div>
+          ) : (
+            cartItems?.map((item, index) => {
+              return (
+                <div key={index} className={styles.single}>
+                  <div className={styles.title}>
+                    <Image width={70} height={70} src={item.image} alt="Product" />
+                    <div>
+                      <h2>{item.title}</h2>
+                      <p>{item.size}</p>
+                      <p>{formatter(item.promoPrice)}</p>
+                    </div>
                   </div>
+                  <p>{formatter(item.promoPrice)}</p>
+                  <div className={styles.quantity}>
+                    <p
+                      onClick={() => {
+                        if (item.count === 1) dispatch(removeFromCart(item));
+                        else dispatch(reducetoCart(item));
+                      }}>
+                      -
+                    </p>
+                    <h4>{item.count}</h4>
+                    <p
+                      onClick={() => {
+                        dispatch(addtoCart(item));
+                      }}>
+                      +
+                    </p>
+                  </div>
+                  <p>{formatter(item.promoPrice * item.count)}</p>
                 </div>
-                <p>{formatter(item.promoPrice)}</p>
-                <div className={styles.quantity}>
-                  <p
-                    onClick={() => {
-                      if (item.count === 1) dispatch(removeFromCart(item));
-                      else dispatch(reducetoCart(item));
-                    }}>
-                    -
-                  </p>
-                  <h4>{item.count}</h4>
-                  <p
-                    onClick={() => {
-                      dispatch(addtoCart(item));
-                    }}>
-                    +
-                  </p>
-                </div>
-                <p>{formatter(item.promoPrice * item.count)}</p>
-              </div>
-            );
-          })}
+              );
+            })
+          )}
         </div>
       </div>
-      <div className={styles.total}>
-        <div>
-          <h4>Total</h4>
-          <p>{formatter(total)}</p>
+      {cartItems.length > 0 && (
+        <div className={styles.total}>
+          <div>
+            <h4>Total</h4>
+            <p>{formatter(total)}</p>
+          </div>
+          <Link href="/checkout">Checkout</Link>
         </div>
-        <Link href="/checkout">Checkout</Link>
-      </div>
+      )}
     </div>
   );
 };
