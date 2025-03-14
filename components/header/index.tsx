ebc6715be3ca854b50fc40867e3c71a5f9760f2c
temfa <akinfetemitope5@gmail.com>
2025-03-14 05:26:22 +0100

    Done

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -37,7 +37,7 @@ const Header = () => {
       </div>
       <div className={styles.action}>
         <p onClick={() => dispatch(setCartOpen(true))}>
-          Cart <sup>{cartItems.length}</sup>
+          Cart {cartItems.length !== 0 && <sup>{cartItems.length}</sup>}
           {/* <CartSvg /> */}
         </p>
         <Link href="/">Book Reservation</Link>
