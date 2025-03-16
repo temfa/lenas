7d7cdc0b570dba0494c8c4df2f4dd2fdf9b9be33
temfa <akinfetemitope5@gmail.com>
2025-03-16 15:47:27 +0100

    Almost Done

--- components/single-product/index.tsx
+++ components/single-product/index.tsx
@@ -31,9 +31,9 @@ const SingleProduct: FC<Props> = ({ image, title, description, size, price, prom
       count: 1,
     };
     dispatch(addtoCart(cartDetails));
-    if (typeof window !== "undefined") {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
+    // if (typeof window !== "undefined") {
+    //   window.scrollTo({ top: 0, behavior: "smooth" });
+    // }
     dispatch(setCartOpen(true));
   };
 
